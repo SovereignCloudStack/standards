2b910162659bcf62e4414fc2b8e98363a3132f61
Kurt Garloff <kurt@garloff.de>
2022-09-15 19:57:04 +0200

    Start testing properties.
    Covered os_* hw_* architecture hypervisor_type for now.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/image-md-check.py
+++ Design-Docs/tools/image-md-check.py
@@ -19,6 +19,7 @@ def usage(rc):
     print("Options: --os-cloud CLOUDNAME: Use this cloud config, default is $OS_CLOUD")
     print(" -p/--private : Also consider private images")
     print(" -v/--verbose : Be more verbose")
+    print(" -s/--skip-completeness: Don't check whether we have all mandatory images")
     print(" -h/--help    : Print this usage information")
     print("If you pass images, only these will be validated, otherwise all images")
     print(" catalog will be processed.")
@@ -27,6 +28,7 @@ def usage(rc):
 # global options
 verbose = False
 private = False
+skip = False
 conn = None
 if "OS_CLOUD" in os.environ:
     cloud = os.environ["OS_CLOUD"]
@@ -46,6 +48,39 @@ def get_imagelist(priv):
         imgs = conn.image.images(visibility='public')
     return list(map(lambda x: x.name, imgs))
 
+class property:
+    def __init__(self, name, mand, values, desc):
+        self.name = name
+        self.ismand = mand
+        self.values = values
+        self.desc = desc
+    def is_ok(self, props, warn = ""):
+        if self.name in props:
+            if self.values and not props[self.name] in self.values:
+                if warn:
+                    print("Error: Image \"%s\": value \"%s\" for property \"%s\" not in list" % \
+                            (warn, props[self.name], self.name))
+                return False
+            else:
+                return True
+        elif self.ismand:
+            if warn:
+                print("Error: Image \"%s\": Mandatory property \"%s\" is missing" % \
+                        (warn, self.name))
+            return False
+        elif warn:
+            print("Info: Image \"%s\": Recommended property \"%s\" is missing" % \
+                    (warn, self.name))
+        return True
+
+os_props = (property("os_distro", True, (), "Operating System"),
+            property("os_version", True, (), "OS Versions"))
+arch_props = (property("architecture", True, ("x86_64", "aarch64", "risc-v"), "CPU architecture"),
+              property("hypervisor_type", True, ("kvm", "xen", "hyper-v", "esxi"), "Hypervisor"))
+hw_props = (property("hw_rng_model", False, ("virtio",), "Random Number Generator"),
+            property("hw_disk_bus", True, ("virtio", "scsi"), "Disk bus: vda or sda"))
+
+
 def validate_imageMD(imgnm):
     try:
         img = conn.image.find_image(imgnm)
@@ -53,13 +88,21 @@ def validate_imageMD(imgnm):
         print("Error with duplicate name \"%s\": %s" % (imgnm, str(exc)), file=sys.stderr)
         return 1
     # Now the hard work: Look at properties ....
+    errors = 0
+    for prop in (*os_props, *arch_props, *hw_props):
+        if not prop.is_ok(img, imgnm):
+            errors += 1
+
     #(1) recommended os_* and hw_*
     #(2) sanity architecture, min_ram, min_disk (vs size)
-    #(3) is_hash
+    #(3) os_hash
     #(4) image_build_date, image_original_user, image_source (opt image_description)
     #(5) maintained_until, provided_until, uuid_validity, update_frequency
+    #(5a) Sanity: Are we actually in violation of update_frquency?
     #(6) tags os:*, managed_by_*
-    return 0
+    if not errors and verbose:
+        print("Image \"%s\": All good" % imgnm)
+    return errors
 
 def report_stdimage_coverage(imgs):
     err = 0
@@ -75,11 +118,11 @@ def report_stdimage_coverage(imgs):
 
 def main(argv):
     # Option parsing
-    global verbose, private
+    global verbose, private, skip
     global cloud, conn
     try:
-        opts, args = getopt.gnu_getopt(argv[1:], "phc:", \
-        	("private", "help", "os-cloud="))
+        opts, args = getopt.gnu_getopt(argv[1:], "phvc:s", \
+           ("private", "help", "os-cloud=", "verbose", "skip-completeness"))
     except getopt.GetoptError as exc:
         usage(1)
     for opt in opts:
@@ -89,6 +132,8 @@ def main(argv):
             private = True
         elif opt[0] == "-v" or opt[0] == "--verbose":
             verbose = True
+        elif opt[0] == "-s" or opt[0] == "--skip-completeness":
+            skip = True
         elif opt[0] == "-c" or opt[0] == "--os-cloud":
             cloud = opt[1]
     images = args
@@ -103,7 +148,9 @@ def main(argv):
     # Analyse image metadata
     for image in images:
         err += validate_imageMD(image)
-    return err + report_stdimage_coverage(images)
+    if not skip:
+        err += report_stdimage_coverage(images)
+    return err
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
