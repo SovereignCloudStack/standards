42e97a49c6a492b3d32b1bd939156de9bb0c214d
Kurt Garloff <kurt@garloff.de>
2022-09-15 12:53:37 +0200

    Get image list and compare to mand/rec standard.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/image-md-check.py
+++ Design-Docs/tools/image-md-check.py
@@ -27,28 +27,43 @@ def usage(rc):
 # global options
 verbose = False
 private = False
-
+conn = None
+if "OS_CLOUD" in os.environ:
+    cloud = os.environ["OS_CLOUD"]
+else:
+    cloud = None
+        
 # Image list
 mand_images = ["Ubuntu 22.04", "Ubuntu 20.04", "Debian 11"]
 rec1_images = ["CentOS 8", "Rocky 8", "Alma Linux 8", "Debian 10", "Fedora 36"]
 rec2_images = ["SLES 15SP4", "RHEL 9", "RHEL 8", "Windows Server 2022", "Windows Server 2019"]
-sugg_images = ["openSUSE Leap 15.4", "Cirros", "Alpine", "Arch"]
+sugg_images = ["openSUSE Leap 15.4", "Cirros 0.5.2", "Alpine", "Arch"]
 
 def get_imagelist(priv):
-    return []
+    return list(map(lambda x: x.name, conn.image.images()))
 
 def validate_imageMD(imgnm):
     return 0
 
 def report_stdimage_coverage(imgs):
-    pass
+    err = 0
+    for inm in mand_images:
+        if inm not in imgs:
+            err += 1
+            print("WARNING: Mandatory image \"%s\" is missing" % inm)
+    for inm in (*rec1_images, *rec2_images):
+        if inm not in imgs:
+            print("INFO: Recommended image \"%s\" is missing" % inm)
+    # Ignore sugg_images for now
+    return err
 
 def main(argv):
     # Option parsing
     global verbose, private
+    global cloud, conn
     try:
         opts, args = getopt.gnu_getopt(argv[1:], "phc:", \
-		("private", "help", "os-cloud="))
+        	("private", "help", "os-cloud="))
     except getopt.GetoptError as exc:
         usage(1)
     for opt in opts:
@@ -58,14 +73,20 @@ def main(argv):
             private = True
         elif opt[0] == "-v" or opt[0] == "--verbose":
             verbose = True
+        elif opt[0] == "-c" or opt[0] == "--os-cloud":
+            cloud = opt[1]
     images = args
+    if not cloud:
+        print("ERROR: Need to specify --os-cloud or set OS_CLOUD environment.", file=sys.stderr)
+        usage(1)
+    conn = openstack.connect(cloud=cloud, timeout=24)
     # Do work
     if not images:
         images = get_imagelist(private)
     # Analyse image metadata
     for image in images:
         validate_imageMD(image)
-    report_stdimage_coverage(images)
+    return report_stdimage_coverage(images)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
