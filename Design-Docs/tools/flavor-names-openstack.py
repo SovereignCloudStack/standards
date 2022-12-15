b8e9b91e53b2e789f2555716490fa975a20edc74
Kurt Garloff <kurt@garloff.de>
2022-12-15 19:17:32 +0100

    Check vCPUs, RAM, Disk OpenStack data vs name.
    Overproviding is allowed, we only warn.
    For underprovisioning, we produce an error.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/flavor-names-openstack.py
+++ Design-Docs/tools/flavor-names-openstack.py
@@ -57,11 +57,53 @@ def main(argv):
 		try:
 			ret = fnmck.parsename(flv.name)
 			assert(ret)
-			if flv.name in fnmck.scsMandatory:
-				fnmck.scsMandatory.remove(flv.name)
-				MSCSFlv.append(flv.name)
+			err = 0
+			warn = 0
+			# vCPUS
+			if (flv.vcpus < ret[0].cpus):
+				print("ERROR: Flavor %s has only %i vCPUs, should have >= %i" % \
+					(flv.name, flv.vcpus, ret[0].cpus), file=sys.stderr)
+				err += 1
+			elif (flv.vcpus > ret[0].cpus):
+				print("WARNING: Flavor %s has %i vCPUs, only needs %i" % \
+					(flv.name, flv.vcpus, ret[0].cpus), file=sys.stderr)
+				warn += 1
+			# RAM
+			flvram = int((flv.ram+51)/102.4)/10
+			if (flvram < ret[0].ram):
+				print("ERROR: Flavor %s has only %.1f GiB RAM, should have >= %.1f GiB" % \
+					(flv.name, flvram, ret[0].ram), file=sys.stderr)
+				err += 1
+			elif (flvram > ret[0].ram):
+				print("WARNING: Flavor %s has %.1f GiB RAM, only needs %.1f GiB" % \
+					(flv.name, flvram, ret[0].ram), file=sys.stderr)
+				warn += 1
+			# DISK
+			accdisk = (0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
+			if not ret[1].parsed:
+				ret[1].disksize = 0
+			if ret[1].disksize not in accdisk:
+				print("WARNING: Flavor %s advertizes disk size %i, should have (5, 10, 20, 50, 100, 200, ...)" % \
+					(flv.name, ret[1].disksize), file=sys.stderr)
+				warn += 1
+			if (flv.disk < ret[1].disksize):
+				print("ERROR: Flavor %s has only %i GB root disk, should have >= %i GB" % \
+					(flv.name, flv.disk, ret[1].disksize), file=sys.stderr)
+				err += 1
+			elif (flv.disk > ret[1].disksize):
+				print("WARNING: Flavor %s has %i GB root disk, only needs %i GB" % \
+					(flv.name, flv.disk, ret[1].disksize), file=sys.stderr)
+				warn += 1
+			if err:
+				wrongFlv.append(flv.name)
 			else:
-				SCSFlv.append(flv.name)
+				if flv.name in fnmck.scsMandatory:
+					fnmck.scsMandatory.remove(flv.name)
+					MSCSFlv.append(flv.name)
+				else:
+					SCSFlv.append(flv.name)
+				if warn:
+					warnFlv.append(flv.name)
 			# TODO: Check OpenStack params vs name
 		except NameError as e:
 			errors += 1
