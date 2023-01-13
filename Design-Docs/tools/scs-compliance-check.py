dde80ef9db1e3d748ce273ad50c8e9a7c6d099bb
Kurt Garloff <kurt@garloff.de>
2023-01-14 00:39:56 +0100

    Implement options and option parsing.
    -q/--quiet
    -v/--verbose
    -d/--date=
    -V/--version-
    -h/--help
    -s/--single-layer
    -c/--os-cloud=
    
    (KUBECTL options missing ...)
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/scs-compliance-check.py
+++ Design-Docs/tools/scs-compliance-check.py
@@ -13,9 +13,9 @@ and performs all the checks for the specified level and outputs a
 verdict from all tests (which is reflected in the exit code)
 """
 
-# import os
+import os
 import sys
-# import getopt
+import getopt
 # import time
 import datetime
 import subprocess
@@ -25,9 +25,12 @@ import yaml
 def usage():
     "Output usage information"
     print("Usage: scs-compliance-check.py [options] compliance-spec.yaml layer [layer [layer]]")
-    # TODO: options: debug, quiet, verbose
-    #  single-layer
-    #  specversion (default: current, other options vX or upcoming or date, etc.)
+    print("Options: -v/--verbose: More verbose output")
+    print(" -q/-quiet: Don't output anythin but errors")
+    print(" -s/--single-layer: Don't perform require checks for dependantr layers")
+    print(" -d/--date YYYY-MM-DD: Check standards valid on specified date instead of today")
+    print(" -V/--version VERS: Force version VERS of the standard (instead of deriving from date)")
+    print(" -c/--os-cloud CLOUD: Use specified cloud env (instead of OS_CLOUD env var)")
 
 
 def is_valid_standard(now, stable, replace):
@@ -57,11 +60,13 @@ def errcode_to_text(err):
         return "PASSED"
     return f"{err} ERRORS"
 
+
 def dictval(dct, key):
     "Helper: Return dct[key] if it exists, None otherwise"
     if key in dct:
         return dct[key]
     return None
+
 
 def search_version(layerdict, checkdate, forceversion=None):
     "Return dict with latest matching version, None if not found"
@@ -78,30 +83,72 @@ def search_version(layerdict, checkdate, forceversion=None):
                 bestdays = diffdays
                 bestversion = versdict
     # print(f"Identified best version {bestversion}")
+    if forceversion and bestversion and not bestversion["version"] == forceversion:
+        print(f"Wanted version {forceversion} which was not found")
+        sys.exit(3)
     return bestversion
+
+
+def optparse(argv):
+    "Parse options. Return (args, verbose, quiet, checkdate, version, singlelayer)."
+    verbose = False
+    quiet = False
+    checkdate = datetime.date.today()
+    version = None
+    single_layer = False
+    try:
+        opts, args = getopt.gnu_getopt(argv, "hvqd:V:sc:",
+                                       ("help", "verbose", "quiet", "date=", "version=",
+                                        "single-layer", "os-cloud="))
+    except getopt.GetoptError as exc:
+        print(f"Option error: {exc}", file=sys.stderr)
+        usage()
+        sys.exit(1)
+    for opt in opts:
+        if opt[0] == "-h" or opt[0] == "--help":
+            usage()
+            sys.exit(0)
+        elif opt[0] == "-v" or opt[0] == "--verbose":
+            verbose = True
+        elif opt[0] == "-q" or opt[0] == "--quiet":
+            quiet = True
+        elif opt[0] == "-d" or opt[0] == "--date":
+            checkdate = datetime.date.fromisoformat(opt[1])
+        elif opt[0] == "-V" or opt[0] == "--version":
+            version = opt[1]
+        elif opt[0] == "-s" or opt[0] == "--single-layer":
+            single_layer = True
+        elif opt[0] == "-c" or opt[0] == "--os-cloud":
+            os.environ["OS_CLOUD"] = opt[1]
+        else:
+            print(f"Error: Unknown argument {opt[0]}", file=sys.stderr)
+    if len(args) < 2:
+        usage()
+        sys.exit(1)
+    return (args, verbose, quiet, checkdate, version, single_layer)
+
 
 def main(argv):
     """Entry point for the checker"""
-    quiet = False
-    versionoverride = None
-    if len(argv) < 2:
-        usage()
-        return 1
-    specfn = argv[0]
-    with open(specfn, "r", encoding="UTF-8") as specfile:
+    args, verbose, quiet, checkdate, version, single_layer = optparse(argv)
+    with open(args[0], "r", encoding="UTF-8") as specfile:
         specdict = yaml.load(specfile, Loader=yaml.SafeLoader)
-    checkdate = datetime.date.today()
     allerrors = 0
-    for layer in argv[1:]:
-        bestversion = search_version(specdict[layer], checkdate, versionoverride)
+    if "depends_on" in specdict and not single_layer:
+        print("WARNING: depends_on not yet implemented!", file=sys.stderr)
+    # Iterate over layers
+    for layer in args[1:]:
+        bestversion = search_version(specdict[layer], checkdate, version)
         if not bestversion:
             print(f"No valid standard found for {checkdate}", file=sys.stderr)
             return 2
         errors = 0
+        if not quiet:
+            print(f"Testing Standard version {bestversion['version']}")
         for standard in bestversion["standards"]:
             if not quiet:
                 print(f"Testing standard {standard['name']} ...")
-            error = run_check_tool(standard["check_tool"])
+            error = run_check_tool(standard["check_tool"], verbose, quiet)
             errors += error
             if not quiet:
                 print(f"... returned {error}")
