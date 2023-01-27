b57562bad781738f06c48faee327f9dce728b95c
Kurt Garloff <kurt@garloff.de>
2023-01-27 19:00:53 +0100

    Feat/linter with external flavors and formatting fixes (#186)
    * Read mand flavors from SCS-Spec.MandatoryFlavors.yaml.
    
    This implements #235.
    
    * Be consistent about using f-strings
    
    * Doc strings, f strings, line-length.
    
    * Extra braces, one more f-string.
    
    * flavor-name-check.py passing flake8 except E226, E501
    
    * flake8 also for flave-names-openstack.py.
    
    Also fix error introduced in Prop parser (when we exhaust the list).
    
    * Use flake8 settings of my liking.
    
    * Fix flake8 complaints (not enough blank lines)
    
    * Fix flake8 complaints and *some* pylint suggestions.
    
    * Use f-strings for formatting to please pylint.
    
    Well, it also pleases my own eyes :-)
    
    * Resolve merge errors.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/flavor-names-openstack.py
+++ Design-Docs/tools/flavor-names-openstack.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # vim: set ts=4 sw=4 et:
-#
-# Flavor naming checker
-# https://github.com/SovereignCloudStack/Docs/Design-Docs/tools
-#
-# Uses the flavor-name-check.py tool
-# Assumes a connection to an OpenStack tenant,
-# retrieves the list of flavors from there and validates them.
-# Something similar could be achieved by:
-# flavor-name-check.py -c $(openstack flavor list -f value -c Name)
-# In addition we check consistency by looking at the information
-# provided by openstack, such as the number of vCPUs and memory.
-#
-# (c) Kurt Garloff <garloff@osb-alliance.com>, 12/2022
-# SPDX-License-Identifier: CC-BY-SA 4.0
+
+"""Flavor naming checker
+https://github.com/SovereignCloudStack/Docs/Design-Docs/tools
+
+Uses the flavor-name-check.py tool
+Assumes a connection to an OpenStack tenant,
+retrieves the list of flavors from there and validates them.
+Something similar could be achieved by:
+flavor-name-check.py -c $(openstack flavor list -f value -c Name)
+In addition we check consistency by looking at the information
+provided by openstack, such as the number of vCPUs and memory.
+
+(c) Kurt Garloff <garloff@osb-alliance.com>, 12/2022
+SPDX-License-Identifier: CC-BY-SA 4.0
+"""
 
 import os
 import sys
@@ -23,7 +24,8 @@ import yaml
 import openstack
 fnmck = importlib.import_module("flavor-name-check")
 
-def usage(rcode = 1):
+
+def usage(rcode=1):
     "help output"
     print("Usage: flavor-names-openstack.py [--os-cloud OS_CLOUD] [-C mand.yaml] [-v] [-q]", file=sys.stderr)
     print(" This tool retrieves the list of flavors from the OpenStack cloud OS_CLOUD", file=sys.stderr)
@@ -31,7 +33,9 @@ def usage(rcode = 1):
     print(" and reports inconsistencies, errors etc. It returns 0 on success.", file=sys.stderr)
     sys.exit(rcode)
 
+
 def main(argv):
+    "Entry point -- main loop going over flavors"
     cloud = None
     verbose = False
     quiet = False
@@ -42,10 +46,10 @@ def main(argv):
     except KeyError:
         pass
     try:
-        opts, args = getopt.gnu_getopt(argv, "c:C:vhq", \
-            ("os-cloud=", "mand=", "verbose", "help", "quiet"))
+        opts, args = getopt.gnu_getopt(argv, "c:C:vhq",
+                                       ("os-cloud=", "mand=", "verbose", "help", "quiet"))
     except getopt.GetoptError as exc:
-        print("%s" % exc, file=sys.stderr)
+        print(f"{exc}", file=sys.stderr)
         usage(1)
     for opt in opts:
         if opt[0] == "-h" or opt[0] == "--help":
@@ -80,7 +84,7 @@ def main(argv):
     errors = 0
     for flv in flavors:
         # Skip non-SCS flavors
-        if flv.name and flv.name[:4] != "SCS-":
+        if flv.name and flv.name[:4] != "SCS-":  # and flv.name[:4] != "SCSx"
             nonSCSFlv.append(flv.name)
             continue
         try:
@@ -98,21 +102,26 @@ def main(argv):
             # see flavor-name-check.py: parsename()
             # vCPUS
             if flv.vcpus < cpuram.cpus:
-                print(f"ERROR: Flavor {flv.name} has only {flv.vcpus} vCPUs, should have >= {cpuram.cpus}", file=sys.stderr)
+                print(f"ERROR: Flavor {flv.name} has only {flv.vcpus} vCPUs, "
+                      f"should have >= {cpuram.cpus}", file=sys.stderr)
                 err += 1
             elif flv.vcpus > cpuram.cpus:
-                print(f"WARNING: Flavor {flv.name} has {flv.vcpus} vCPUs, only needs {cpuram.cpus}", file=sys.stderr)
+                print(f"WARNING: Flavor {flv.name} has {flv.vcpus} vCPUs, "
+                      f"only needs {cpuram.cpus}", file=sys.stderr)
                 warn += 1
             # RAM
             flvram = int((flv.ram + 51) / 102.4) / 10
             # Warn for strange sizes (want integer numbers, half allowed for < 10GiB)
             if flvram >= 10 and flvram != int(flvram) or flvram * 2 != int(flvram * 2):
-                print(f"WARNING: Flavor {flv.name} uses discouraged uneven size of memory {flvram:.1f} GiB", file=sys.stderr)
+                print(f"WARNING: Flavor {flv.name} uses discouraged uneven size "
+                      f"of memory {flvram:%.1f} GiB", file=sys.stderr)
             if flvram < cpuram.ram:
-                print(f"ERROR: Flavor {flv.name} has only {flvram:.1f} GiB RAM, should have >= {cpuram.ram:.1f} GiB", file=sys.stderr)
+                print(f"ERROR: Flavor {flv.name} has only {flvram:.1f} GiB RAM, "
+                      f"should have >= {cpuram.ram:.1f} GiB", file=sys.stderr)
                 err += 1
             elif flvram > cpuram.ram:
-                print(f"WARNING: Flavor {flv.name} has {flvram:.1f} GiB RAM, only needs {cpuram.ram:.1f} GiB", file=sys.stderr)
+                print(f"WARNING: Flavor {flv.name} has {flvram:.1f} GiB RAM, "
+                      f"only needs {cpuram.ram:.1f} GiB", file=sys.stderr)
                 warn += 1
             # DISK
             accdisk = (0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
@@ -121,13 +130,16 @@ def main(argv):
                 disk.disksize = 0
             # We have a recommendation for disk size steps
             if disk.disksize not in accdisk:
-                print(f"WARNING: Flavor {flv.name} advertizes disk size {disk.disksize}, should have (5, 10, 20, 50, 100, 200, ...)", file=sys.stderr)
+                print(f"WARNING: Flavor {flv.name} advertizes disk size {disk.disksize}, "
+                      f"should have (5, 10, 20, 50, 100, 200, ...)", file=sys.stderr)
                 warn += 1
             if flv.disk < disk.disksize:
-                print(f"ERROR: Flavor {flv.name} has only {flv.disk} GB root disk, should have >= {disk.disksize} GB", file=sys.stderr)
+                print(f"ERROR: Flavor {flv.name} has only {flv.disk} GB root disk, "
+                      f"should have >= {disk.disksize} GB", file=sys.stderr)
                 err += 1
             elif flv.disk > disk.disksize:
-                print(f"WARNING: Flavor {flv.name} has {flv.disk} GB root disk, only needs {disk.disksize} GB", file=sys.stderr)
+                print(f"WARNING: Flavor {flv.name} has {flv.disk} GB root disk, "
+                      f"only needs {disk.disksize} GB", file=sys.stderr)
                 warn += 1
             # Ev'thing checked, react to errors by putting the bad flavors in the bad bucket
             if err:
@@ -145,7 +157,7 @@ def main(argv):
         except NameError as exc:
             errors += 1
             wrongFlv.append(flv.name)
-            print("Wrong flavor \"%s\": %s" % (flv.name, exc), file=sys.stderr)
+            print(f"Wrong flavor \"{flv.name}\": {exc}", file=sys.stderr)
     # This makes the output more readable
     MSCSFlv.sort()
     SCSFlv.sort()
@@ -181,12 +193,14 @@ def main(argv):
         "Errors": errors,
         "Warnings": len(warnFlv)
     }
-    Report = {cloud: {"SCSFlavorSummary": flvSCSRep, "OtherFlavorSummary": flvOthRep,
-                      "TotalSummary": totSummary}}
+    Report = {cloud: {"TotalSummary": totSummary}}
+    if not quiet:
+        Report[cloud]["SCSFlavorSummary"] = flvSCSRep
+        Report[cloud]["OtherFlavorSummary"] = flvOthRep
     if verbose:
         Report[cloud]["SCSFlavorReport"] = flvSCSList
         Report[cloud]["OtherFlavorReport"] = flvOthList
-    print("%s" % yaml.dump(Report, default_flow_style=False))
+    print(f"{yaml.dump(Report, default_flow_style=False)}")
     return errors
 
 
