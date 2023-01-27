c58a4d0706042b04a5b6ba697c10e91763d2ec1f
Eduard Itrich <eduard@itrich.net>
2023-01-27 15:17:25 +0100

    Write result of compliance check to given path (#187)
    * Write result of compliance check to given path
    
    * Remove 'result' from list of accepted keywords.
    
    * Report name of cloud, results -> errors.
    
    A few mnior improvements:
    - Change the name of the results field to errors, so it's clear
      that 0 is good and non-zero is bad.
    - Move checked_at date to the top level of report
    - Add os_cloud to report, so we know what has been tested.
    
    * pylint and flake8 suggestions implemented.
    
    This is cosmetics.
    
    * Create copy.deepcopy() before adding errors.
    
    This avoids the errors dict key to be added to the original version
    spec. (Adding it would create a warning from an unknown keyword errors.)
    
    This will make the version_index = report[...].index(bestversion) fail.
    As we don't have bestversion in report, but a deep copy, that is to be
    expected.
    
    We fortunately don't need to determine the index, it's always 0 (as we
    only copy this one).
    
    Signed-off-by: Eduard Itrich <eduard@itrich.net>
    Signed-off-by: Kurt Garloff <kurt@garloff.de>
    Co-authored-by: Kurt Garloff <kurt@garloff.de>

--- Design-Docs/tools/scs-compliance-check.py
+++ Design-Docs/tools/scs-compliance-check.py
@@ -28,6 +28,7 @@ import getopt
 # import time
 import datetime
 import subprocess
+import copy
 import yaml
 
 
@@ -40,6 +41,7 @@ def usage():
     print(" -d/--date YYYY-MM-DD: Check standards valid on specified date instead of today")
     print(" -V/--version VERS: Force version VERS of the standard (instead of deriving from date)")
     print(" -c/--os-cloud CLOUD: Use specified cloud env (instead of OS_CLOUD env var)")
+    print(" -o/--output path: Generate report of compliance check under given path")
 
 
 def is_valid_standard(now, stable, obsolete):
@@ -105,16 +107,17 @@ def search_version(layerdict, checkdate, forceversion=None):
 
 
 def optparse(argv):
-    "Parse options. Return (args, verbose, quiet, checkdate, version, singlelayer)."
+    "Parse options. Return (args, verbose, quiet, checkdate, version, singlelayer, output)."
     verbose = False
     quiet = False
     checkdate = datetime.date.today()
     version = None
     single_layer = False
+    output = None
     try:
-        opts, args = getopt.gnu_getopt(argv, "hvqd:V:sc:",
+        opts, args = getopt.gnu_getopt(argv, "hvqd:V:sc:o:",
                                        ("help", "verbose", "quiet", "date=", "version=",
-                                        "single-layer", "os-cloud="))
+                                        "single-layer", "os-cloud=", "output="))
     except getopt.GetoptError as exc:
         print(f"Option error: {exc}", file=sys.stderr)
         usage()
@@ -135,20 +138,29 @@ def optparse(argv):
             single_layer = True
         elif opt[0] == "-c" or opt[0] == "--os-cloud":
             os.environ["OS_CLOUD"] = opt[1]
+        elif opt[0] == "-o" or opt[0] == "--output":
+            output = opt[1]
         else:
             print(f"Error: Unknown argument {opt[0]}", file=sys.stderr)
     if len(args) < 2:
         usage()
         sys.exit(1)
-    return (args, verbose, quiet, checkdate, version, single_layer)
+    return (args, verbose, quiet, checkdate, version, single_layer, output)
 
 
 def main(argv):
     """Entry point for the checker"""
-    args, verbose, quiet, checkdate, version, single_layer = optparse(argv)
+    args, verbose, quiet, checkdate, version, single_layer, output = optparse(argv)
     with open(args[0], "r", encoding="UTF-8") as specfile:
         specdict = yaml.load(specfile, Loader=yaml.SafeLoader)
     allerrors = 0
+    report = {}
+    if output:
+        for key in "name", "url":
+            report[key] = dictval(specdict, key)
+        report["os_cloud"] = os.environ["OS_CLOUD"]
+        # TODO: Add kubeconfig context as well
+        report["checked_at"] = checkdate
     if "depends_on" in specdict and not single_layer:
         print("WARNING: depends_on not yet implemented!", file=sys.stderr)
     # Iterate over layers
@@ -163,6 +175,9 @@ def main(argv):
         if "standards" not in bestversion:
             print(f"WARNING: No standards defined yet for {layer} version {bestversion['version']}",
                   file=sys.stderr)
+        if output:
+            report[layer] = {}
+            report[layer]["versions"] = [copy.deepcopy(bestversion)]
         for standard in bestversion["standards"]:
             optional = False
             optstr = ""
@@ -180,18 +195,25 @@ def main(argv):
             else:
                 args = dictval(standard, 'check_tool_args')
                 error = run_check_tool(standard["check_tool"], args, verbose, quiet)
+                if output:
+                    version_index = 0  # report[layer]["versions"].index(bestversion)
+                    standard_index = bestversion["standards"].index(standard)
+                    report[layer]["versions"][version_index]["standards"][standard_index]["errors"] = error
             if not optional:
                 errors += error
             if not quiet and "check_tool" in standard:
-                print(f"... returned {error}")
+                print(f"... returned {error} errors")
             for kwd in standard:
                 if kwd not in ('check_tool', 'check_tool_args', 'url', 'name', 'condition'):
                     print(f"ERROR in spec: standard.{kwd} is an unknown keyword", file=sys.stderr)
-        # TODO: Option to write output-report.yaml
+        if output:
+            report[layer]["versions"][version_index]["errors"] = errors
+            with open(output, 'w', encoding='UTF-8') as file:
+                output = yaml.safe_dump(report, file, default_flow_style=False, sort_keys=False)
         if not quiet:
             print("*******************************************************")
-        print(f"Verdict for cloud {os.environ['OS_CLOUD']}, layer {layer}, "
-              f"version {bestversion['version']}: {errcode_to_text(errors)}")
+            print(f"Verdict for os_cloud {os.environ['OS_CLOUD']}, layer {layer}, "
+                  f"version {bestversion['version']}: {errcode_to_text(errors)}")
         allerrors += errors
     return allerrors
 
