ea596b6d4a79675f275b71f60d9dcefe921f83bb
Max Wolfs <mail@maxwolfs.com>
2023-03-03 01:57:19 +0100

    update sidebar to new community docs structure (#53)
    Signed-off-by: maxwolfs <mail@maxwolfs.com>

--- sidebarsCommunity.js
+++ sidebarsCommunity.js
@@ -5,15 +5,34 @@ const sidebars = {
   community: [
     'index',
     'calendar',
+    'community/collaboration',
     {
       type: 'category',
-      label: 'Contribute',
+      label: 'Communication',
       items: [
-        'contribute/collaboration',
-        'contribute/license-considerations',
-        'contribute/dco-and-licenses'
+        'community/communication/matrix'
       ]
-    }
+    },
+    {
+      type: 'category',
+      label: 'Cloud Resources',
+      items: [
+        'community/cloud-resources/cloud-resources',
+        'community/cloud-resources/getting-started-openstack',
+        'community/cloud-resources/plusserver-gx-scs',
+        'community/cloud-resources/wavestack'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'GitHub',
+      items: [
+        'community/github/branchprotection',
+        'community/github/dco-and-licenses',
+        'community/github/tips-and-tricks'
+      ]
+    },
+    'community/license-considerations'
   ]
 }
 
