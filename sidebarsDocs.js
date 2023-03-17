4508f98deb9aa0b84cf965f8f03057fb1556b096
Max Wolfs <mail@maxwolfs.com>
2023-03-17 11:36:27 +0100

    Add further documentations (#58)
    * add ciab and testbed docs
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * add k8s cluster api provider docs
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * improve order
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    ---------
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -6,6 +6,63 @@ const sidebarsDocs = {
     'intro',
     {
       type: 'category',
+      label: 'K8s Cluster API Provider',
+      link: {
+        type: 'generated-index'
+      },
+      items: [
+        'k8s-cluster-api-provider/doc/overview',
+        'k8s-cluster-api-provider/doc/requirements',
+        'k8s-cluster-api-provider/doc/quickstart',
+        'k8s-cluster-api-provider/doc/getting-started',
+        'k8s-cluster-api-provider/doc/application-credentials',
+        'k8s-cluster-api-provider/doc/configuration',
+        'k8s-cluster-api-provider/doc/Maintenance_and_Troubleshooting',
+        'k8s-cluster-api-provider/doc/LoadBalancer-ExtTrafficLocal',
+        'k8s-cluster-api-provider/doc/Upgrade-Guide',
+        'k8s-cluster-api-provider/doc/teardown',
+        {
+          type: 'category',
+          label: 'Usage',
+          items: [
+            'k8s-cluster-api-provider/doc/usage/usage',
+            'k8s-cluster-api-provider/doc/usage/create-new-cluster',
+            'k8s-cluster-api-provider/doc/usage/managing-many-clusters',
+            'k8s-cluster-api-provider/doc/usage/cluster-mgmt-capi-mgmt-node',
+            'k8s-cluster-api-provider/doc/usage/multi-az-and-multi-cloud-environments',
+            'k8s-cluster-api-provider/doc/usage/testing'
+          ]
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'OSISM Testbed',
+      link: {
+        type: 'generated-index'
+      },
+      items: [
+        'testbed/doc/overview',
+        'testbed/doc/requirements',
+        'testbed/doc/preparations',
+        'testbed/doc/quickstart',
+        'testbed/doc/getting_started',
+        'testbed/doc/authentication',
+        'testbed/doc/configuration',
+        'testbed/doc/networking',
+        'testbed/doc/usage',
+        'testbed/doc/important_notes',
+        'testbed/doc/contribute',
+        'testbed/doc/license',
+        {
+          type: 'category',
+          label: 'Cloud in a Box',
+          items: ['testbed/cloud-in-a-box/doc/quickstart']
+        }
+      ]
+    },
+    {
+      type: 'category',
       label: 'Openstack Image Manager',
       link: {
         type: 'generated-index'
