e4173b4675766287e159193c0b5e631dc3a8157a
Hannes Baum <hannes.baum@cloudandheat.com>
2023-06-13 14:36:39 +0200

    Update Entropy Test due to Standard Change (#268)
    * adapt to the new standard introduced in #297
    * added specific exceptions
    * small inline documentation
    * added return codes and explanation
    * better explained log messages
    
    Signed-off-by: Hannes Baum <hannes.baum@cloudandheat.com>

--- Tests/iaas/entropy/entropy-check.py
+++ Tests/iaas/entropy/entropy-check.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python3
-"""
+"""Entropy checker
+
+Return codes:
+0:    Enough entropy is available, no errors or warnings during execution
+
+1:    The cloud parameter wasn't provided
+2:    The used parameters were incorrect
+3:    No connection to the OpenStack Cloud possible
+
+10:   At least one VM doesn't have the required fixed entropy available
+11:   At least one VM had too many FIPS 140-2 failures during the test
+19:   Error during the VM requirements check routine
+
+20:   At least one image is missing the recommended HWRNG attributes
+29:   Error during the image attribute check routine
+
+30:   At least one flavor is missing the recommended HWRNG attributes
+31:   At least one flavor doesn't have the optionally recommended HWRNG attributes
+39:   Error during the flavor attribute check routine
+
+40:   At least one VM doesn't provide the recommended RNG daemon
+49:   Error during the VM service check routine
+
+All return codes between (and including) 1-19 as well as all return codes ending on 9
+can be seen as failures.
+
 Check given cloud for conformance with SCS standard regarding
 entropy, to be found under /Standards/scs-0101-v1-entropy.md
 """
 import errno
 import getopt
 import os
+import re
 import sys
 import time
 
-import openstack
 import fabric
+import keystoneauth1.exceptions
+import openstack
+from openstack.cloud import exc
+
+server_name = "test"
+security_group_name = "test-group"
+keypair_name = "test-keypair"
 
 
 def print_usage(file=sys.stderr):
@@ -23,128 +55,105 @@ This tool retrieves the list of flavors from the OpenStack cloud OS_CLOUD
 """, end='', file=file)
 
 
-def get_kernel_version(kernel):
-    nv = kernel.split(" ")[1]
-    nvd = nv.split("-")
-    _v = nvd[0].split(".")
-
-    version = {
-        'major': int(_v[0]),
-        'minor': int(_v[1]),
-        'patch': int(_v[2]),
-        'pre-release': int(nvd[1])
-    }
-    return version
+class InternalProcessException(Exception):
+    """Raised when an error happens during an internal process"""
+    pass
 
 
-def print_results(results, theme):
-    print(f"{theme}")
-    for key, value in results.items():
-        print(f"{key}")
-        for k, v in value.items():
-            if v:
-                if k in ["version", "rng_available"]:
-                    print(f"    - {k} {v}")
-                else:
-                    print(f"    \33[32m- {k} ✓\33[0m")
-            else:
-                print(f"    \33[31m- {k} ×\33[0m")
+def check_image_attributes(conn):
+    return_code = 0
 
+    image_attributes = ["hw_rng_model"]
 
-def test_vm(server_image):
-    # We need to sleep here, to get the server completely up
-    time.sleep(30)
-
-    host = server_image[0]['public_v4']
-    user = server_image[1]['properties']['standarduser']
-    try:
-        fconn = fabric.Connection(host=host, user=user, connect_kwargs={"key_filename": "./key.priv"})
-        kernel_version = fconn.run('uname -smr', hide=True).stdout
-    except Exception as e:
-        raise e
-
-    return kernel_version
-
-
-def test_flavors(conn):
-    flavor_list = dict()
-    list_to_test = ("hw_rng:allowed", "hw_rng:rate_bytes", "hw_rng:rate_period")
-    for flavor in conn.compute.flavors():
-        flavor_info = conn.compute.get_flavor(flavor['id'], get_extra_specs=True)
-        flavor_info = flavor_info.to_dict()
-        flavor_dict = dict()
-        for k, v in flavor_info['extra_specs'].items():
-            for ltt in list_to_test:
-                if k == ltt and (v is not False or v is not None):
-                    flavor_dict[ltt] = True
-                else:
-                    flavor_dict[ltt] = False
-        flavor_list[flavor_info['name']] = flavor_dict
-    print_results(flavor_list, "Flavors")
-
-
-def test_images(conn):
-    image_list = dict()
     for image in conn.image.images():
         image_info = conn.image.get_image(image['id'])
         image_info = image_info.to_dict()
-        if image_info['hw_rng_model'] is not None:
-            image_list[image_info['name']] = {"hw_rng_model": True}
+
+        if image_info[image_attributes[0]] is not None and \
+                image_info[image_attributes[0]] == "virtio":
+            pass
         else:
-            image_list[image_info['name']] = {"hw_rng_model": False}
-    print_results(image_list, "Images")
+            print(f"INFO: Image {image_info['name']} doesn't have recommended attribute {image_attributes[0]}")
+            return_code = 20
+    return return_code
 
 
-def test_services(server_image):
-    service_list = dict()
+def check_flavor_attributes(conn):
+    return_code = 0
 
-    host = server_image[0]['public_v4']
-    user = server_image[1]['properties']['standarduser']
+    flavor_attributes = ["hw_rng:allowed", "hw_rng:rate_bytes", "hw_rng:rate_period"]
+
+    for flavor in conn.compute.flavors():
+        flavor_info = conn.compute.get_flavor(flavor['id'], get_extra_specs=True)
+        flavor_info = flavor_info.to_dict()
+
+        for k, v in flavor_info['extra_specs'].items():
+            for attr in flavor_attributes:
+                if k == attr and v:
+                    pass
+                else:
+                    print(f"INFO: Flavor {flavor_info['name']} doesn't have recommended attribute {attr}")
+                    if attr == "hw_rng:allowed":
+                        return_code = 30
+                    else:
+                        return_code = 31
+    return return_code
+
+
+def check_vm_requirements(host, user):
     try:
         fconn = fabric.Connection(host=host, user=user, connect_kwargs={"key_filename": "./key.priv"})
 
-        service_list['kernel'] = dict()
-        try:
-            service_list['kernel']['version'] = fconn.run('uname -smr', hide=True).stdout.replace("\n", "")
-        except BaseException:
-            service_list['kernel']['version'] = False
+        entropy_avail = fconn.run('cat /proc/sys/kernel/random/entropy_avail', hide=True).stdout
 
-        service_list['haveged'] = dict()
+        _ = fconn.run('sudo apt-get update && sudo apt-get install rng-tools', hide=True).stdout
         try:
-            service_list['haveged']['status'] = fconn.run('sudo systemctl status haveged', hide=True).stdout
-        except BaseException:
-            service_list['haveged']['status'] = False
-
-        service_list['rng-tools'] = dict()
-        try:
-            service_list['rng-tools']['status'] = fconn.run('sudo systemctl status rng-tools', hide=True).stdout
-        except BaseException:
-            service_list['rng-tools']['status'] = False
-
-        service_list['hw-random'] = dict()
-        try:
-            hwr = fconn.run('cat /sys/devices/virtual/misc/hw_random/rng_available', hide=True).stdout.replace("\n", "")
-            service_list['hw-random']['rng_available'] = hwr
-        except BaseException:
-            service_list['hw-random']['rng_available'] = False
+            fips_data = fconn.run('cat /dev/random | rngtest -c 1000', hide=True).stderr
+        except Exception as e:
+            fips_data = e.result.stderr
+            pass
     except Exception as e:
-        raise e
+        raise InternalProcessException(str(e))
+    finally:
+        fconn.close()
 
-    print_results(service_list, "Services")
+    if int(entropy_avail) != 256:
+        print(f"ERROR: The tested VM doesn't have a fixed amount of entropy available. "
+              f"Excepted 256, got {entropy_avail}.")
+        return str(5)
+
+    failure_re = re.search(r'failures:\s\d+', fips_data, flags=re.MULTILINE)
+    fips_failures = failure_re.string[failure_re.regs[0][0]:failure_re.regs[0][1]].split(" ")[1]
+    if int(fips_failures) > 3:
+        print(f"ERROR: The tested VM didn't pass the FIPS 140-2 testing."
+              f"Excepted a maximum of 3 failures, got {fips_data}.")
+        return 11
+    return 0
 
 
-def test_routine(conn, server_image):
-    kernel_version = test_vm(server_image)
+def check_services(host, user):
+    try:
+        fconn = fabric.Connection(host=host, user=user, connect_kwargs={"key_filename": "./key.priv"})
 
-    kernel_version = get_kernel_version(kernel_version)
-    if (kernel_version['major'] == 5 and kernel_version['minor'] < 18) or \
-       (kernel_version['major'] < 5):
-        test_services(server_image)
+        service_status = fconn.run('sudo systemctl status rngd', hide=True).stdout
+        if "could not be found" in service_status:
+            print(f"INFO: VM {host} doesn't provide the recommended service rngd")
+            return 30
+    except Exception as e:
+        if "could not be found" in e.result.stderr:
+            print(f"INFO: VM {host} doesn't provide the recommended service rngd")
+            return 30
+        else:
+            raise InternalProcessException(str(e))
+    finally:
+        fconn.close()
+    return 0
 
 
 def create_vm(conn):
     try:
-        keypair = conn.compute.create_keypair(name='test')
+        # Create a keypair and save both parts for later usage
+        keypair = conn.compute.create_keypair(name=keypair_name)
 
         try:
             os.mkdir("./")
@@ -157,9 +166,9 @@ def create_vm(conn):
             f.close()
         os.chmod('key.priv', 0o400)
 
-        # ---------------------------------------------------------------------
+        # Create a new security group and give it some simple rules in order to access it via SSH
         example_sec_group = conn.network.create_security_group(
-            name='test-group'
+            name=security_group_name
         )
 
         _ = conn.network.create_security_group_rule(
@@ -180,7 +189,9 @@ def create_vm(conn):
             port_range_min=22,
             ethertype='IPv4',
         )
-        # ---------------------------------------------------------------------
+
+        # Pick an image and a flavor and create a server with both of them and
+        # the previously created keys and security group
         image = None
         flavor = None
         for i in conn.image.images():
@@ -194,45 +205,56 @@ def create_vm(conn):
             j += 1
             if j == 2:
                 break
-        server = conn.create_server('test', image=image, flavor=flavor, key_name=keypair.name,
+
+        server = conn.create_server(server_name, image=image, flavor=flavor, key_name=keypair.name,
                                     security_groups=['test-group'], wait=True, auto_ip=True)
+
         return [server.to_dict(), image.to_dict()]
-    except Exception as e:
+    except exc.OpenStackCloudException as e:
         raise e
 
 
 def delete_vm(conn):
     try:
-        _ = conn.delete_server('test', wait=True)
-    except BaseException:
+        _ = conn.delete_server(server_name, wait=True)
+    except exc.OpenStackCloudException:
+        print(f"DEBUG: The server {server_name} couldn't be deleted.")
         pass
+
     try:
         for sg in conn.network.security_groups():
             sg = sg.to_dict()
             if sg['name'] == "test-group":
                 _ = conn.network.delete_security_group(sg['id'])
-    except BaseException:
+    except (exc.OpenStackCloudException, exc.OpenStackCloudUnavailableFeature):
+        print(f"DEBUG: The security group {security_group_name} couldn't be deleted.")
         pass
+
     try:
         fips = conn.list_floating_ips()
         for fip in fips:
             if fip.status == 'DOWN':
                 _ = conn.delete_floating_ip(fip.id, retry=5)
-    except BaseException:
+    except exc.OpenStackCloudException:
+        print(f"DEBUG: The floating_ip {fip} couldn't be deleted.")
         pass
+
     try:
         os.remove("./key.priv")
-    except BaseException:
+    except OSError:
+        print("DEBUG: The key file 'key.priv' couldn't be deleted.")
         pass
+
     try:
-        _ = conn.compute.delete_keypair('test')
-    except BaseException:
+        _ = conn.compute.delete_keypair(keypair_name)
+    except exc.OpenStackCloudException:
+        print(f"DEBUG: The keypair {keypair_name} couldn't be deleted.")
         pass
 
 
 def main(argv):
     try:
-        opts, args = getopt.gnu_getopt(argv, "c:h", ("os-cloud=", "help", ))
+        opts, args = getopt.gnu_getopt(argv, "c:h", ["os-cloud=", "help"])
     except getopt.GetoptError as exc:
         print(f"{exc}", file=sys.stderr)
         print_usage()
@@ -251,22 +273,65 @@ def main(argv):
 
     if not cloud:
         print("ERROR: You need to have OS_CLOUD set or pass --os-cloud=CLOUD.", file=sys.stderr)
-        # optionally also: print_usage()
         return 1
 
-    conn = openstack.connect(cloud=cloud, timeout=32)
     try:
-        # the standard prescribes certain properties for every flavor (Section 1.3.1) and every image (Section 1.3.2)
-        test_flavors(conn)
-        test_images(conn)
-        # next, the standard demands that rng-utils be present and active on compute nodes (Section 1.3.3)
-        # (FIXME the following does check something else?)
-        si = create_vm(conn)
-        test_routine(conn, si)
+        conn = openstack.connect(cloud=cloud, timeout=32)
+    except keystoneauth1.exceptions.MissingRequiredOptions:
+        print("ERROR: Connection to the OpenStack cloud wasn't possible.")
+        return 3
+
+    return_code = 0
+
+    # Check the images for HWRNG attributes (RECOMMENDED)
+    try:
+        return_code = check_image_attributes(conn)
     except BaseException:
+        return_code = 19
+        pass
+
+    # Check the flavors for HWRNG attributes (RECOMMENDED)
+    try:
+        rc = check_flavor_attributes(conn)
+        if rc != 0:
+            return_code = rc
+    except BaseException:
+        return_code = 29
+        pass
+
+    # Check a VM for services and requirements
+    try:
+        si = create_vm(conn)
+
+        # Sleep, in order for the VM to show up completely
+        time.sleep(30)
+
+        host = si[0]['public_v4']
+        user = si[1]['properties']['standarduser']
+
+        # Check the VM for running services (RECOMMENDED)
+        try:
+            rc = check_services(host, user)
+            if rc != 0:
+                return_code = rc
+        except InternalProcessException:
+            return_code = 39
+            pass
+
+        # Check the VM for specific requirements (REQUIRED)
+        try:
+            rc = check_vm_requirements(host, user)
+            if rc != 0:
+                return_code = rc
+        except InternalProcessException:
+            return_code = 19
+            pass
+    except (BaseException, InternalProcessException, exc.OpenStackCloudException):
         pass
     finally:
         delete_vm(conn)
+
+    return return_code
 
 
 if __name__ == "__main__":
