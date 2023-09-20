cfd363a224d557b166ca100b5c2ecc28506ead0f
Markus Hentsch <markus.hentsch@secustack.com>
2023-09-20 18:02:00 +0200

    Add more tests for groups functionality
    Signed-off-by: Markus Hentsch <markus.hentsch@secustack.com>

--- Tests/iam/domain-manager/domain-manager-check.py
+++ Tests/iam/domain-manager/domain-manager-check.py
@@ -117,7 +117,7 @@ def test_logins(cloud_name: str, domains: list[dict]):
 
 def cleanup(cloud_name: str, domains: list[dict]):
     """
-    Uses the configured domain manager user to cleanup any Keystone resources
+    Uses the configured domain manager users to cleanup any Keystone resources
     that this test suite might have created in the corresponding domains
     during any previous or current execution.
     Resources are discovered within the domains configured for testing only
@@ -153,40 +153,200 @@ def cleanup(cloud_name: str, domains: list[dict]):
                 conn.identity.delete_user(user.id)
 
 
+def _raisesException(exception, func, *args, **kwargs):
+    try:
+        func(*args, **kwargs)
+    except exception as e:
+        return True
+    else:
+        return False
+
+
 def test_groups(cloud_name: str, domains: list[dict]):
 
-    # 1st domain
+    # 1st domain = D1
     domain_a = domains[0].get("name")
     conn_a = connect_to_domain(cloud_name, domain_a, domains)
     domain_a_id = conn_a.identity.find_domain(domain_a).id
+    domain_a_user = conn_a.identity.create_user(
+        name="scs-test-domain-a-user-1",
+        domain_id=domain_a_id
+    )
 
-    # 2nd domain
+    # 2nd domain = D2
     domain_b = domains[1].get("name")
     conn_b = connect_to_domain(cloud_name, domain_b, domains)
-    domain_b_id = conn_a.identity.find_domain(domain_a).id
+    domain_b_id = conn_b.identity.find_domain(domain_b).id
+    domain_b_user = conn_b.identity.create_user(
+        name="scs-test-domain-b-user-1",
+        domain_id=domain_b_id
+    )
 
-    # group creation outside of domain (negative test)
-    try:
-        conn_a.identity.create_group(
-            name="scs-test-group-outside-domain-a",
-        )
-    except openstack.exceptions.ForbiddenException:
-        print("Domain manager cannot create group outside of domain 1: PASS")
-    else:
-        raise Exception(
-            f"Policy error: domain manager can create group outside of domain "
-            f"'{domain_a}'"
-        )
+    # [D1] group creation without specifying domain (negative test)
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.create_group,
+        name="scs-test-group-outside-domain-a"
+    ), (
+        f"Policy error: domain manager can create group without "
+        f"specifying domain '{domain_a}'"
+    )
+    print("Domain manager cannot create group without specifying domain: PASS")
 
-    # group creation within domain
+    # [D1] group creation in a foreign domain (negative test)
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.create_group,
+        name="scs-test-group-in-wrong-domain",
+        domain_id=domain_b_id
+    ), (
+        f"Policy error: domain manager of '{domain_a}' can create group "
+        f"in foreign domain '{domain_b}'"
+    )
+    print("Domain manager cannot create group in foreign domain: PASS")
+
+    # [D1] group creation within domain
     domain_a_group = conn_a.identity.create_group(
-        name="scs-test-group-outside-domain-a",
+        name="scs-test-group-inside-domain-a",
         domain_id=domain_a_id
     )
     assert domain_a_group, (
         f"Domain manager cannot create groups within domain '{domain_a}'"
     )
-    print("Domain manager can create group within domain 1: PASS")
+    print("Domain manager can create group within domain: PASS")
+
+    # [D2] domain manager does not see group of foreign domain D1
+    domain_a_groups = conn_b.identity.groups()
+    assert next(domain_a_groups, None) is None, (
+        f"Policy error: domain manager of '{domain_b}' can see groups of "
+        f"domain '{domain_a}'"
+    )
+    print("Domain manager cannot see groups of foreign domain: PASS")
+
+    # [D2] domain manager cannot update group of foreign domain D1
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_b.identity.update_group,
+        domain_a_group.id,
+        name="scs-test-group-RENAMED"
+    ), (
+        f"Policy error: domain manager of '{domain_b}' can update group in "
+        f"foreign domain '{domain_a}'"
+    )
+    print("Domain manager cannot update group in foreign domain: PASS")
+
+    # [D2] domain manager cannot delete group of foreign domain D1
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_b.identity.delete_group,
+        domain_a_group.id
+    ), (
+        f"Policy error: domain manager of '{domain_b}' can delete group in "
+        f"foreign domain '{domain_a}'"
+    )
+    print("Domain manager cannot delete group in foreign domain: PASS")
+
+    # [D2] group creation within domain (prerequisite for subsequent tests)
+    domain_b_group = conn_b.identity.create_group(
+        name="scs-test-group-inside-domain-b",
+        domain_id=domain_b_id
+    )
+    assert domain_b_group, (
+        f"Domain manager cannot create groups within domain '{domain_b}'"
+    )
+
+    # [D1] domain manager can query group relationship of user within domain
+    assert not _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.check_user_in_group,
+        domain_a_user.id, domain_a_group.id
+    ), (
+        f"Policy error: domain manager of '{domain_a}' cannot use "
+        f"check_user_in_group within domain"
+    )
+    print("Domain manager can use check_user_in_group within domain: PASS")
+
+    # [D1] domain manager cannot query group relationship of user when
+    # group or user are in foreign domain D2 (negative test)
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.check_user_in_group,
+        domain_b_user.id, domain_a_group.id
+    ), (
+        f"Policy error: domain manager of '{domain_a}' can use "
+        f"check_user_in_group for user of foreign domain '{domain_b}'"
+    )
+    print("Domain manager cannot use check_user_in_group "
+          "for user of foreign domain: PASS")
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.check_user_in_group,
+        domain_a_user.id, domain_b_group.id
+    ), (
+        f"Policy error: domain manager of '{domain_a}' can use "
+        f"check_user_in_group for group of foreign domain '{domain_b}'"
+    )
+    print("Domain manager cannot use check_user_in_group "
+          "for group of foreign domain: PASS")
+    assert _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.check_user_in_group,
+        domain_b_user.id, domain_b_group.id
+    ), (
+        f"Policy error: domain manager of '{domain_a}' can use "
+        f"check_user_in_group for user and group, both of foreign domain "
+        f"'{domain_b}'"
+    )
+    print("Domain manager cannot use check_user_in_group "
+          "for user and group of foreign domain: PASS")
+
+    # [D1] domain manager can add user to group within domain
+    assert not _raisesException(
+        openstack.exceptions.ForbiddenException,
+        conn_a.identity.add_user_to_group,
+        domain_a_user.id, domain_a_group.id
+    ), (
+        f"Policy error: domain manager cannot add user to group within domain "
+        f"'{domain_a}'"
+    )
+    assert conn_a.identity.check_user_in_group(
+        domain_a_user.id, domain_a_group.id
+    ), (
+        f"User '{domain_a_user.name}' was not successfully added to group "
+        f"'{domain_a_group.name}' in domain '{domain_a}'"
+    )
+    print("Domain manager can add user to group within domain: PASS")
+
+    # [D1] domain manager cannot add user to group across domain boundaries
+    # TODO: user a + group b
+    # TODO: user b + group a
+    # TODO: user b + group b
+
+    # [D1] domain manager can list groups for user
+    # TODO
+
+    # [D1] domain manager can assign role to group within domain
+    # TODO: assign_project_role_to_group(), assign_domain_role_to_group()
+
+    # [D1] domain manager can revoke role from group within domain
+    # TODO: unassign_project_role_from_group(), unassign_domain_role_from_group
+
+    # [D1] domain manager can delete group within domain
+    # TODO
+
+    # [D1] domain manager cannot query user lists of groups of foreign domains
+    # TODO: group_users(domain_b_group.id)
+
+    # [D1] domain manager cannot remove user from group in foreign domain
+    # TODO: remove_user_from_group(domain_b_user.id, domain_b_group.id)
+
+    # [D1] domain manager cannot delete group in foreign domain
+    # TODO: delete_group(domain_b_group.id)
+
+    # [D1] domain cannot assign role to group in foreign domain
+    # TODO: assign_project_role_to_group()
+
+    # TODO: system role assignments?
 
 
 def main():
