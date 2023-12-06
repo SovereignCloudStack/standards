abab48bce34095c90fde087b5c74cd07a36bb789
Kurt Garloff <kurt@garloff.de>
2023-12-06 16:52:09 +0100

    Avoid any HTML tags in flavor name.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- Tests/iaas/flavor-naming/flavor-form.py
+++ Tests/iaas/flavor-naming/flavor-form.py
@@ -28,7 +28,10 @@ ERROR = ""
 def parse_name(fnm):
     "return tuple with flavor description"
     global FLAVOR_SPEC, FLAVOR_NAME, ERROR
-    fnm = re.sub(r"<( *script)", r"<!--\1", fnm, flags=re.I)
+    # Sanitize
+    #fnm = re.sub(r"<( *script)", r"<!--\1", fnm, flags=re.I)
+    fnm = re.sub(r"<", r"&lt;", fnm)
+    fnm = re.sub(r">", r"&gt;", fnm)
     FLAVOR_NAME = fnm
     try:
         FLAVOR_SPEC = fnmck.parsename(fnm)
