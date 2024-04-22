6f2b7b0473a9afe0f15ee62437e2a8fdb72876fc
josephineSei <128813814+josephineSei@users.noreply.github.com>
2024-04-22 15:02:10 +0200

    Restructure Tests to allow the special ingress case but only for default security groups.
    Signed-off-by: josephineSei <128813814+josephineSei@users.noreply.github.com>

--- Tests/iaas/security-groups/default-security-group-rules.py
+++ Tests/iaas/security-groups/default-security-group-rules.py
@@ -35,6 +35,7 @@ def test_rules(cloud_name: str):
 
     # count all overall ingress rules and egress rules.
     ingress_rules = 0
+    ingress_from_same_sg = 0
     egress_rules = 0
     if not rules:
         print("No default security group rules defined.")
@@ -42,13 +43,20 @@ def test_rules(cloud_name: str):
         for rule in rules:
             if rule.direction == "ingress":
                 ingress_rules += 1
+                # we allow ingress from the same security group
+                # but only for the default security group
+                if (rule.remote_group_id == "PARENT" and 
+                  not rule.used_in_non_default_sg):
+                    ingress_from_same_sg += 1
             elif rule.direction == "egress":
                 egress_rules += 1
 
-    # test whether there are no ingress_rules allowed
-    assert ingress_rules == 0, (
-        f"expected 0 default ingress rules, "
-        f"but there are {ingress_rules}")
+    # test whether there are no other than the allowed ingress rules
+    assert ingress_rules == ingress_from_same_sg, (
+        f"Expected only ingress rules for default security groups, "
+        f"that allow ingress traffic from the same group. "
+        f"But there are more - in total {ingress_rules} ingress rules. "
+        f"There should be only {ingress_from_same_sg} ingress rules.")
 
     result_dict = {
         "Ingress Rules": ingress_rules,
