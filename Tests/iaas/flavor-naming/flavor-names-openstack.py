b49fe42e867873eeaf5a4dbd6f8f052d9b658e10
Matthias Büchse <matthias.buechse@cloudandheat.com>
2024-08-02 11:05:34 +0200

    remove misleading output from scs-0100 check
    Signed-off-by: Matthias Büchse <matthias.buechse@cloudandheat.com>

--- Tests/iaas/flavor-naming/flavor-names-openstack.py
+++ Tests/iaas/flavor-naming/flavor-names-openstack.py
@@ -218,10 +218,15 @@ def main(argv):
     }
     flvSCSRep = {
         "TotalAmount": len(MSCSFlv) + len(SCSFlv) + len(wrongFlv),
-        "MandatoryFlavorsPresent": len(MSCSFlv),
-        "MandatoryFlavorsMissing": len(scsMandatory),
     }
-    if v3mode:
+    # skip the following if no mandatory flavors are given (useful for v3.2 onward)
+    if len(MSCSFlv) + len(scsMandatory):
+        flvSCSRep.update({
+            "MandatoryFlavorsPresent": len(MSCSFlv),
+            "MandatoryFlavorsMissing": len(scsMandatory),
+        })
+    # skip the following if no recommended flavors are given (useful for v1, v2, and v3.2 onward)
+    if len(RSCSFlv) + len(scsRecommended):
         flvSCSRep.update({
             "RecommendedFlavorsPresent": len(RSCSFlv),
             "RecommendedFlavorsMissing": len(scsRecommended),
