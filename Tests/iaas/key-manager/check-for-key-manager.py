4458d9a20a99ef54c7c493221e5c96eec55f92e7
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-09-19 12:31:02 +0200

    alternating between auth methods
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

--- Tests/iaas/key-manager/check-for-key-manager.py
+++ Tests/iaas/key-manager/check-for-key-manager.py
@@ -26,25 +26,24 @@ def connect(cloud_name: str) -> openstack.connection.Connection:
         cloud=cloud_name,
     )
 
-def delete_application_credential(conn: openstack.connection.Connection,
-                                  credential_name: str) -> None:
+
+def delete_application_credential(
+    conn: openstack.connection.Connection, credential_name: str
+) -> None:
     existing_credential = conn.identity.find_application_credential(
-        conn.current_user_id,
-        credential_name
+        conn.current_user_id, credential_name
     )
+
     if existing_credential:
-        print(
-            f"INFO: deleting application credential "
-            f"'{credential_name}' ..."
-        )
+        print(f"INFO: deleting application credential '{credential_name}' ...")
         conn.identity.delete_application_credential(
-            conn.current_user_id,
-            existing_credential
+            conn.current_user_id, existing_credential
         )
 
-def reconnect_with_role(conn: openstack.connection.Connection,
-                        target_role_name: str
-                        ) -> openstack.connection.Connection:
+
+def reconnect_with_role(
+    conn: openstack.connection.Connection, target_role_name: str, cloud_name
+) -> openstack.connection.Connection:
     """
     Uses the existing cloud connection to create a new application credential
     in the Identity API limited to the role specified via target_role_name.
@@ -52,26 +51,28 @@ def reconnect_with_role(conn: openstack.connection.Connection,
     returns it, effectively scoping the returned connection to the specific
     role.
     """
-    credential_name = APP_CREDENTIAL_NAME
+    credential_name = cloud_name
     delete_application_credential(conn, credential_name)
     app_credential = conn.identity.create_application_credential(
-        conn.current_user_id,
-        credential_name,
-        roles=[{"name": target_role_name}]
+        conn.current_user_id, credential_name, roles=[{"name": target_role_name}]
     )
-
     # Open a new connection using the application credential
     new_conn = openstack.connect(
-        region_name=conn.config.config["region"],
+        region_name="RegionOne",  # conn.config.config["region_name"],
         auth_type="v3applicationcredential",
         auth={
             "auth_url": conn.auth["auth_url"],
             "application_credential_id": app_credential.id,
+            "application_credential_secret": app_credential.secret
         },
     )
+    print(f"reconnected with {credential_name}")
     return new_conn
 
-def check_for_member_role(conn: openstack.connection.Connection) -> None:
+
+def check_for_member_role(
+    conn: openstack.connection.Connection, cloud_name: str
+) -> None:
     """Checks whether the current user has at maximum privileges
     of the member role.
     :param connection:
@@ -79,49 +80,18 @@ def check_for_member_role(conn: openstack.connection.Connection) -> None:
     :returns: boolean, when role with most priviledges is member
     """
 
-    auth_data = conn.auth
+    #  # test alternative role function
+    #   roles = conn.identity.roles()
+    #   for role in roles:
+    #     print (role.name)
+    #     if role.name == "member":
+    #         member_role = role
+    #         print(member_role)
+    #         break
 
-    auth_dict = {
-        "identity": {
-            "methods": ["password"],
-            "password": {
-                "user": {
-                    "name": auth_data["username"],
-                    "domain": {"name": auth_data["project_domain_name"]},
-                    "password": auth_data["password"],
-                }
-            },
-        },
-        "scope": {
-            "project": {
-                "domain": {"name": auth_data["project_domain_name"]},
-                "name": auth_data["project_name"],
-            }
-        },
-    }
-
-    has_member_role = False
-
-    # check whether auth_url already has v3
-    if "/v3/" in auth_data['auth_url']:
-      auth_url = auth_data['auth_url'] + 'auth/tokens'
-    else:
-      auth_url = auth_data['auth_url'] + '/v3/auth/tokens'
-
-    print(f"!URL {auth_url}")
-    request = conn.session.request(auth_url, "POST", json={"auth": auth_dict})
-    #print(request.content)
-
-    # # test alternative role function
-    # roles = conn.identity.roles()
-    # for role in roles:
-    #   print (role.name)
-    #   if role.name == "member":
-    #       member_role = role
-    #       print(member_role)
-    #       break
-
-    # # Check if the user has the "member" role
+    # Check if the user has the "member" role
+    # user_id = "aa0304f7017144ceaadad5e0c26238e8"
+    # project_id = "50cbe6c8b6af45cba53f3500a236cebf"
     # role_assignments = conn.identity.role_assignments(user=user_id, project=project_id)
     # has_member_role = False
     # for assignment in role_assignments:
@@ -129,7 +99,59 @@ def check_for_member_role(conn: openstack.connection.Connection) -> None:
     #     if role.name == "member":
     #         has_member_role = True
     #         break
+    auth_data = conn.auth
+    print(auth_data)
 
+    new_conn=reconnect_with_role(conn,"member", cloud_name)
+    auth_data = new_conn.auth
+
+    print(auth_data)
+    # auth_dict = {
+    #     "identity": {
+    #         "methods": ["password"],
+    #         "password": {
+    #             "user": {
+    #                 "name": auth_data["username"],
+    #                 "domain": {"name": auth_data["project_domain_name"]},
+    #                 "password": auth_data["password"],
+    #             }
+    #         },
+    #     },
+    #     "scope": {
+    #         "project": {
+    #             "domain": {"name": auth_data["project_domain_name"]},
+    #             "name": auth_data["project_name"],
+    #         }
+    #     },
+    # }
+    auth_dict = {
+
+                  "identity": {
+                      "methods": ["application_credential"],
+                      "application_credential": {
+                          "id": auth_data["application_credential_id"],
+                          "secret": auth_data["application_credential_secret"]
+                      }
+                  },
+        # "scope": {
+        #     "project": {
+        #         "domain": {"name": auth_data["project_domain_name"]},
+        #         "name": auth_data["project_name"],
+        #     }
+        # },
+    }
+
+    has_member_role = False
+
+    # check whether auth_url already has v3
+    if "/v3/" in auth_data["auth_url"]:
+        auth_url = auth_data["auth_url"] + "auth/tokens"
+    else:
+        auth_url = auth_data["auth_url"] + "/v3/auth/tokens"
+
+    print(f"!URL {auth_url}")
+    request = conn.session.request(auth_url, "POST", json={"auth": auth_dict})
+    # print(request.content)
 
     # working original test
     for role in json.loads(request.content)["token"]["roles"]:
@@ -153,11 +175,12 @@ def check_presence_of_key_manager(cloud_name: str):
         services = connection.service_catalog
     except Exception as e:
         print(str(e))
-        raise Exception(
-            f"Connection to cloud '{cloud_name}' was not successfully. "
-            f"The Catalog endpoint could not be accessed. "
-            f"Please check your cloud connection and authorization."
-        )
+        reconnect_with_role(None, "member", cloud_name)
+        # raise Exception(
+        #     f"Connection to cloud '{cloud_name}' was not successfully. "
+        #     f"The Catalog endpoint could not be accessed. "
+        #     f"Please check your cloud connection and authorization."
+        # )
 
     for svc in services:
         svc_type = svc["type"]
@@ -165,7 +188,8 @@ def check_presence_of_key_manager(cloud_name: str):
             # key-manager is present
             # now we want to check whether a user with member role
             # can create and access secrets
-            check_key_manager_permissions(connection)
+            print("Key-Manager is present")
+            check_key_manager_permissions(connection, cloud_name)
             return 0
 
     # we did not find the key-manager service
@@ -174,14 +198,16 @@ def check_presence_of_key_manager(cloud_name: str):
     return 0
 
 
-def check_key_manager_permissions(conn: openstack.connection.Connection) -> None:
+def check_key_manager_permissions(
+    conn: openstack.connection.Connection, cloud_name
+) -> None:
     """
     After checking that the current user only has the member and maybe the
     reader role, this method verifies that the user with a member role
     has sufficient access to the Key Manager API functionality.
     """
     secret_name = "scs-member-role-test-secret"
-    if not check_for_member_role(conn):
+    if not check_for_member_role(conn, cloud_name):
         logger.warning("Cannot test key-manager permissions. " "User has wrong roles")
         return None
 
