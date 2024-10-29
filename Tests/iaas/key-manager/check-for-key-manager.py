e1f03e6c5fd4fb5878080831c96746923c197b73
Matthias Büchse <matthias.buechse@cloudandheat.com>
2024-10-29 10:24:32 +0100

    Revised structure, logging, error handling, return code, documentation
    Signed-off-by: Matthias Büchse <matthias.buechse@cloudandheat.com>

--- Tests/iaas/key-manager/check-for-key-manager.py
+++ Tests/iaas/key-manager/check-for-key-manager.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Key Manager service checker
+"""Key Manager service checker for scs-0116-v1-key-manager-standard.md
+
 This script retrieves the endpoint catalog from Keystone using the OpenStack
 SDK and checks whether a key manager API endpoint is present.
-It then checks, whether a user with the maximum of a member role can create secrets.
+It then checks whether a user with the maximum of a member role can create secrets.
 This will only work after policy adjustments or with the new secure RBAC roles and policies.
 The script relies on an OpenStack SDK compatible clouds.yaml file for
 authentication with Keystone.
@@ -11,6 +12,7 @@ authentication with Keystone.
 import argparse
 import logging
 import os
+import sys
 
 import openstack
 
@@ -18,23 +20,23 @@ logger = logging.getLogger(__name__)
 
 
 def initialize_logging():
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 
 def check_for_member_role(conn: openstack.connection.Connection) -> None:
-    """Checks whether the current user has at maximum privileges
-    of the member role.
-    :param connection:
-        The current connection to an OpenStack cloud.
-    :returns: boolean, when role with most priviledges is member
+    """Checks whether the current user has at maximum privileges of the member role.
+
+    :param conn: connection to an OpenStack cloud.
+    :returns: boolean, when role with most privileges is member
     """
     role_names = set(conn.session.auth.get_access(conn.session).role_names)
     if role_names & {"admin", "manager"}:
         return False
     if "reader" in role_names:
         logger.info("User has reader role.")
-    if role_names - {"reader", "member"}:
-        logger.info("User has custom role.")
+    custom_roles = sorted(role_names - {"reader", "member"})
+    if custom_roles:
+        logger.info(f"User has custom roles {', '.join(custom_roles)}.")
     return "member" in role_names
 
 
@@ -42,11 +44,8 @@ def check_presence_of_key_manager(conn: openstack.connection.Connection) -> None
     try:
         services = conn.service_catalog
     except Exception as e:
-        logger.error(str(e))
-        raise Exception(
-            "The Catalog endpoint could not be accessed. "
-            "Please check your cloud connection and authorization."
-        )
+        loggger.critical("Could not access Catalog endpoint.")
+        raise
 
     for svc in services:
         svc_type = svc["type"]
@@ -54,13 +53,21 @@ def check_presence_of_key_manager(conn: openstack.connection.Connection) -> None
             # key-manager is present
             # now we want to check whether a user with member role
             # can create and access secrets
-            logger.info("Key-Manager is present")
-            check_key_manager_permissions(conn)
-            return
+            logger.info("Key Manager is present")
+            return True
 
-    # we did not find the key-manager service
-    logger.warning("There is no key-manager endpoint in the cloud.")
-    # we do not fail, until a key-manager MUST be present
+
+def _find_secret(conn: openstack.connection.Connection, secret_name_or_id: str):
+    """Replacement method for finding secrets.
+
+    Mimicks the behavior of Connection.key_manager.find_secret()
+    but fixes an issue with the internal implementation raising an
+    exception due to an unexpected microversion parameter.
+    """
+    secrets = conn.key_manager.secrets()
+    for s in secrets:
+        if s.name == secret_name_or_id or s.id == secret_name_or_id:
+            return s
 
 
 def check_key_manager_permissions(conn: openstack.connection.Connection) -> None:
@@ -70,25 +77,8 @@ def check_key_manager_permissions(conn: openstack.connection.Connection) -> None
     has sufficient access to the Key Manager API functionality.
     """
     secret_name = "scs-member-role-test-secret"
-    if not check_for_member_role(conn):
-        logger.warning("Cannot test key-manager permissions. User has wrong roles")
-        return None
-
-    def _find_secret(secret_name_or_id: str):
-        """Replacement method for finding secrets.
-
-        Mimicks the behavior of Connection.key_manager.find_secret()
-        but fixes an issue with the internal implementation raising an
-        exception due to an unexpected microversion parameter.
-        """
-        secrets = conn.key_manager.secrets()
-        for s in secrets:
-            if s.name == secret_name_or_id or s.id == secret_name_or_id:
-                return s
-        return None
-
     try:
-        existing_secret = _find_secret(secret_name)
+        existing_secret = _find_secret(conn, secret_name)
         if existing_secret:
             conn.key_manager.delete_secret(existing_secret)
 
@@ -98,25 +88,20 @@ def check_key_manager_permissions(conn: openstack.connection.Connection) -> None
             secret_type="opaque",
             payload="foo",
         )
-        new_secret = _find_secret(secret_name)
-
-        if not new_secret:
-            raise ValueError(
-                f"Secret created with name '{secret_name}' was not discoverable by the user"
-            )
-        conn.key_manager.delete_secret(new_secret)
-
+        try:
+            new_secret = _find_secret(conn, secret_name)
+            if not new_secret:
+                raise ValueError(f"Secret '{secret_name}' was not discoverable by the user")
+        finally:
+            conn.key_manager.delete_secret(new_secret)
     except openstack.exceptions.ForbiddenException as e:
-        logger.info(
-            "Users with the 'member' role can use Key Manager API: FAIL "
-            "- According to the Key Manager Standard, users with the "
-            "'member' role should be able to use the Key Manager API."
+        logger.debug('exception details', exc_info=True)
+        logger.error(
+            "Users with the 'member' role can use Key Manager API: FAIL"
         )
-        logger.error(f"ERROR: {str(e)}")
-        exit(1)
+        return 1
     logger.info(
-        "Users with the 'member' role can use Key Manager API: PASS "
-        "- This is compliant to the Key Manager Standard."
+        "Users with the 'member' role can use Key Manager API: PASS"
     )
 
 
@@ -133,7 +118,11 @@ def main():
         "--debug", action="store_true", help="Enable OpenStack SDK debug logging"
     )
     args = parser.parse_args()
-    openstack.enable_logging(debug=args.debug)
+    # @mbuechse: I think this is so much as to be unusable!
+    # (If necessary, a developer can always uncomment)
+    # openstack.enable_logging(debug=args.debug)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
 
     # parse cloud name for lookup in clouds.yaml
     cloud = args.os_cloud or os.environ.get("OS_CLOUD", None)
@@ -144,8 +133,21 @@ def main():
         )
 
     with openstack.connect(cloud=cloud) as conn:
-        return check_presence_of_key_manager(conn)
+        if not check_for_member_role(conn):
+            logger.critical("Cannot test key-manager permissions. User has wrong roles")
+            return 1
+        if check_presence_of_key_manager(conn):
+            return check_key_manager_permissions(conn)
+        else:
+            # not an error, because key manager is merely recommended
+            logger.warning("There is no key-manager endpoint in the cloud.")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        sys.exit(main())
+    except SystemExit:
+        raise
+    except BaseException:
+        logger.critical("exception", exc_info=True)
+        sys.exit(1)
