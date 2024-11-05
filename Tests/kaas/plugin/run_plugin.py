37b8018484e3f785b5bfa145ec73e710ce204739
Toni Finger <toni.finger@cloudandheat.com>
2024-11-05 16:56:13 +0100

    Fixup
    Add plugin_config to delete process as well
    Add missing file `kind_cluster_config.yaml` as configuration example
    
    Signed-off-by: Toni Finger <toni.finger@cloudandheat.com>

--- Tests/kaas/plugin/run_plugin.py
+++ Tests/kaas/plugin/run_plugin.py
@@ -18,21 +18,7 @@ def init_plugin(plugin_kind, config_path):
     plugin_maker = PLUGIN_LOOKUP.get(plugin_kind)
     if plugin_maker is None:
         raise ValueError(f"unknown plugin '{plugin_kind}'")
-<<<<<<< HEAD
     return plugin_maker(config_path)
-=======
-    return plugin_maker(config)
-
-
-def run_plugin_create(plugin_kind, plugin_config, clusterspec_cluster, clusterspec):
-    plugin = init_plugin(plugin_kind, plugin_config)
-    plugin.create(clusterspec_cluster, clusterspec[clusterspec_cluster]['branch'], os.path.abspath(clusterspec[clusterspec_cluster]['kubeconfig']))
-
-
-def run_plugin_delete(plugin_kind, clusterspec_cluster, clusterspec):
-    plugin = init_plugin(plugin_kind)
-    plugin.delete(clusterspec_cluster)
->>>>>>> Apply cluster configuration file handling
 
 
 def load_spec(clusterspec_path):
@@ -49,19 +35,12 @@ def cli():
 @click.argument('plugin_kind', type=click.Choice(list(PLUGIN_LOOKUP), case_sensitive=False))
 @click.argument('plugin_config', type=click.Path(exists=True, dir_okay=False))
 @click.argument('clusterspec_path', type=click.Path(exists=True, dir_okay=False))
-<<<<<<< HEAD
 @click.argument('cluster_id', type=str, default="default")
 def create(plugin_kind, plugin_config, clusterspec_path, cluster_id):
     clusterspec = load_spec(clusterspec_path)['clusters']
     plugin = init_plugin(plugin_kind, plugin_config)
     clusterinfo = clusterspec[cluster_id]
     plugin.create_cluster(cluster_id, clusterinfo['branch'], os.path.abspath(clusterinfo['kubeconfig']))
-=======
-@click.argument('clusterspec_cluster', type=str, default="default")
-def create(plugin_kind, plugin_config, clusterspec_path, clusterspec_cluster):
-    clusterspec = load_spec(clusterspec_path)['clusters']
-    run_plugin_create(plugin_kind, plugin_config, clusterspec_cluster, clusterspec)
->>>>>>> Apply cluster configuration file handling
 
 
 @cli.command()
