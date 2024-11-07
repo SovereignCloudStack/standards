cfa113c717153e9429a50ab730d0e5b10a6b8ec1
Toni Finger <toni.finger@cloudandheat.com>
2024-11-07 10:10:58 +0100

    Remove abstract base clase handling and directly make use of Not ImplementedError
    Signed-off-by: Toni Finger <toni.finger@cloudandheat.com>

--- Tests/kaas/plugin/interface.py
+++ Tests/kaas/plugin/interface.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 
 
 class KubernetesClusterPlugin():
@@ -37,7 +36,6 @@ class KubernetesClusterPlugin():
         ..
     """
 
-    @abstractmethod
     def create_cluster(self, cluster_name="scs-cluster", version=None, kubeconfig_filepath=None) -> (str):
         """
         This method is to be called to create a k8s cluster
@@ -46,10 +44,11 @@ class KubernetesClusterPlugin():
         :param: kubeconfig_filepath:
         :return: kubeconfig_filepath
         """
+        raise NotImplementedError
 
-    @abstractmethod
     def delete_cluster(self, cluster_name=None):
         """
         This method is to be called in order to unprovision a cluster
         :param: cluster_name:
         """
+        raise NotImplementedError
