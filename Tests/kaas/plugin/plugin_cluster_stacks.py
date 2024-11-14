9fcbf2d51dd133d9c491aee6c0790c52e6c06abe
Michal Gubricky <michal.gubricky@dnation.cloud>
2024-11-14 13:29:38 +0100

    Update Tests/kaas/plugin/plugin_cluster_stacks.py
    Co-authored-by: Matthias Büchse <matthias.buechse@cloudandheat.com>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>

--- Tests/kaas/plugin/plugin_cluster_stacks.py
+++ Tests/kaas/plugin/plugin_cluster_stacks.py
@@ -93,8 +93,6 @@ def load_config(config_path):
     """
     Loads the configuration from a YAML file.
     """
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Configuration file {config_path} not found.")
 
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file) or {}
