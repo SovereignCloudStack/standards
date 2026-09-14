f116ae0c7d5ecd9734cd6e5c2d9d932552f7439c
Matthias Büchse <matthias.buechse@alasca.cloud>
2026-09-14 09:33:57 +0200

    Fix sonobuoy reporting (#1277)
    amending f9fbdaf
    
    Signed-off-by: Matthias Büchse <matthias.buechse@alasca.cloud>

--- Tests/kaas/k8s_test.py
+++ Tests/kaas/k8s_test.py
@@ -94,11 +94,12 @@ class Config:
 
 
 def run_sono(config, testcase, *args):
-    return SonobuoyHandler(
+    return_code = SonobuoyHandler(
         SCS_SONOBUOY_CONFIG_PATH, testcase, config.kubeconfig_path,
         result_dir_name=os.path.join(os.path.dirname(config.kubeconfig_path), testcase),
         args=config.compute_sono_args(*args),
     ).run()
+    return return_code == 0
 
 
 def harness(name, results, *check_fns):
